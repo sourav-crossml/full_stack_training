84860f2224d56a724c0adf04b1dd6cfcbba97ce5
sourav <soura@crossml.com>
2022-05-30 17:31:16 +0530

    updates

--- Atlas/atlasapp/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-# urls of app
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('datatable', views.datatable,name='datatable'),
-]
