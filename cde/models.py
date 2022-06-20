006c74d2cb0f3f1850e58edd1c55b1b247c4fd1e
sourav <souravsingh5880@gmail.com>
2022-06-21 00:45:37 +0530

    UI updates of new Process

---
+++ cde/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+pipeline_select_options = (
+    (0,'Engine-I'),
+    (1,'Engine-II')
+)
+
+classification_model_select_options = (
+    (0,'Engine-I'),
+    (1,'Engine-II')
+)
+
+
+
+
+
+# Create your models here.
+class NewProcess(models.Model):
+    process_name = models.CharField(max_length=30)
+    pipeline = models.CharField(max_length=1,choices=pipeline_select_options)
+    classication_model = models.CharField(max_length=1,choices=classification_model_select_options)
+
+    
