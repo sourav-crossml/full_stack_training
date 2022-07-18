74fbc42eef75c5f512e19895f6c6c3cc9a4c92fb
sourav-crossml <88368353+sourav-crossml@users.noreply.github.com>
2022-07-18 10:40:06 +0530

    Delete e_commerce directory

--- e_commerce/users/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Customer
-# Register your models here.
-admin.site.register(Customer)
--- e_commerce/users/urls.py
+++
@@ -1,7 +0,0 @@
-from unicodedata import name
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('register', views.Registeruser.as_view(),name='register'),
-    path('login', views.login_user,name='login')
-]
